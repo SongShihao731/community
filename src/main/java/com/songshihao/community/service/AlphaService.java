dbeda40f997777c92626016d4e51420153b51cf3
SongShihao731 <74278211+SongShihao731@users.noreply.github.com>
2022-05-06 17:06:52 +0800

    声明式事务管理和编程式事务管理的测试

--- src/main/java/com/songshihao/community/service/AlphaService.java
+++ src/main/java/com/songshihao/community/service/AlphaService.java
@@ -1,12 +1,26 @@
 package com.songshihao.community.service;
 
 import com.songshihao.community.dao.AlphaDao;
+import com.songshihao.community.dao.DiscussPostMapper;
+import com.songshihao.community.dao.UserMapper;
+import com.songshihao.community.entity.DiscussPost;
+import com.songshihao.community.entity.User;
+import com.songshihao.community.util.CommunityUtil;
+import javafx.beans.binding.ObjectExpression;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Date;
 
 @Service
 //@Scope("prototype")
@@ -14,6 +28,15 @@ public class AlphaService {
 
     @Autowired
     private AlphaDao alphaDao;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     // 无参构造器
     public AlphaService() {
@@ -35,5 +58,74 @@ public class AlphaService {
     // 模拟一个业务方法，去调用alphaDao
     public String find() {
         return alphaDao.select();
+    }
+
+    // 模拟一个业务方法来进行事务的管理
+    // Transactional 注解告诉spring这是个事务
+    // isolation 选择隔离等级
+    // propagation传播机制(假设情境：A调用B)
+    // REQUIRED:支持当前事务（A就是当前事务，外部事务），如果外部事务不存在，则创建新事务
+    // REQUITED_NEW：创建一个新事务（B），并且暂停当前事务（A）
+    // NESTED：如果存在当前事务（即外部事务A），则嵌套在该事务中执行（B嵌套在A中，B有独立的提交和回滚），否则就和REQUIRED
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public Object save1() {
+        // 新增用户
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+        user.setEmail("alpha@qq.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        // 新增帖子
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("Hello!");
+        post.setContent("新人报道！");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        // 人为造错，观察事务回滚
+        Integer.valueOf("abc");
+
+        return "ok";
+    }
+
+    // 模拟一个业务方法模拟编程式事务管理
+    public Object save2() {
+        // 设置隔离等级
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        // 设置传播等级
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                // 新增用户
+                User user = new User();
+                user.setUsername("beta");
+                user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+                user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+                user.setEmail("beta@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                // 新增帖子
+                DiscussPost post = new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("你好!");
+                post.setContent("我是新人！");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+
+                // 人为造错，观察事务回滚
+                Integer.valueOf("abc");
+
+                return "ok";
+            }
+        });
     }
 }
---
+++ src/test/java/com/songshihao/community/TransactionTests.java
@@ -0,0 +1,27 @@
+package com.songshihao.community;
+
+import com.songshihao.community.service.AlphaService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class TransactionTests {
+
+    @Autowired
+    private AlphaService alphaService;
+
+    @Test
+    public void testSave1() {
+        Object o = alphaService.save1();
+        System.out.println(o);
+    }
+
+    @Test
+    public void testSave2() {
+        Object o = alphaService.save2();
+        System.out.println(o);
+    }
+}
