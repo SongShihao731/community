906900bdf961661ba060d0725af81242aa955093
SongShihao731 <74278211+SongShihao731@users.noreply.github.com>
2022-05-31 11:13:07 +0800

    降低了springboot的版本，降低到2.1.5 RELEASE 以便整合elasticsearch

--- src/main/java/com/songshihao/community/CommunityApplication.java
+++ src/main/java/com/songshihao/community/CommunityApplication.java
@@ -3,8 +3,17 @@ package com.songshihao.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CommunityApplication {
+
+	@PostConstruct
+	public void init() {
+		// 解决netty启动冲突问题 redis 底层也是netty elasticsearch底层会和redis抢netty占用
+		// see Netty4Utils.serAvailableProcessors()
+		System.setProperty("es.set.netty.runtime.available.processors", "false");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
--- src/test/java/com/songshihao/community/JSONTest.java
+++ src/test/java/com/songshihao/community/JSONTest.java
@@ -3,11 +3,14 @@ package com.songshihao.community;
 
 import com.songshihao.community.util.CommunityUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 import java.util.Map;
+
 
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
